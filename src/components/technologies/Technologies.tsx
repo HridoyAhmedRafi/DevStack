7e91d2e4f0cb75cd9833cd2139a11a5f7461567e
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-11 16:35:45 +0600

    Technologies card design complete

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import type { TechnologiesType } from "../../types/TechnologieType";
+import TechnologiesCard from "./TechnologiesCard";
 
 interface TechnologiesPromiseProps {
   TechnologiesPromise: Promise<TechnologiesType[]>;
@@ -7,8 +8,30 @@ interface TechnologiesPromiseProps {
 
 const Technologies = ({ TechnologiesPromise }: TechnologiesPromiseProps) => {
   const Technologies = use(TechnologiesPromise);
-  console.log(Technologies);
-  return <div></div>;
+
+  return (
+    <div>
+      <div className="container mx-auto px-4">
+        <h1 className="text-[36px] font-bold">
+          Explore the{" "}
+          <span className="bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent">
+            Technologies
+          </span>
+        </h1>
+        <p className="text-[16px] text-[#c3c3c3f0]">
+          Pick one technology per category to build your ideal stack.
+        </p>
+        <div className="grid grid-cols-3 gap-5 my-10">
+          {Technologies.map((Technologie) => (
+            <TechnologiesCard
+              Technologie={Technologie}
+              key={Technologie.id}
+            ></TechnologiesCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Technologies;
---
+++ src/components/technologies/TechnologiesCard.tsx
@@ -0,0 +1,45 @@
+import type { TechnologiesType } from "../../types/TechnologieType";
+
+interface TechnologiesCardProps {
+  Technologie: TechnologiesType;
+}
+
+const TechnologiesCard = ({ Technologie }: TechnologiesCardProps) => {
+  return (
+    <div>
+      <div className="border border-[#90929533] rounded-[13px] ">
+        <div className="py-4 px-4 space-y-3">
+          <div className="flex justify-between ">
+            <img
+              src={Technologie.icon}
+              alt={Technologie.name}
+              className="w-10 h-10"
+            />
+            <p className="border border-[#90929533] px-2 py-1 rounded-2xl">
+              {Technologie.badge}
+            </p>
+          </div>
+          <div>
+            <p className="text-[18px] text-[#0F172A] font-bold">
+              {Technologie.name}
+            </p>
+            <p className="text-[#64748B] text-[15px]">
+              {Technologie.description}
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p className="bg-[#F1F5F9] text-[#475569] px-3 py-1 rounded">
+              {Technologie.category}
+            </p>
+            <p className="text-[#64748B]">{Technologie.difficulty}</p>
+            <span>⭐{Technologie.rating}</span>
+          </div>
+          <button className="bg-[#0A0F1D] text-white w-full py-3 px-2 rounded-[13px] cursor-pointer ">Add to Stack</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechnologiesCard;
