ca1dd4936a38eaec45b788e5ae23086259d205fa
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-11 11:52:18 +0600

    navber sticky position modified

--- src/Navber.tsx
+++ src/Navber.tsx
@@ -3,7 +3,7 @@ import Logo from "../src/assets/logo-text.png";
 const Navber = () => {
   return (
     <div className="border-b border-[#90929533]">
-      <nav className="flex items-center justify-between container mx-auto px-4 py-4 ">
+      <nav className="flex items-center justify-between container mx-auto px-4 py-4 fixed top-0 left-0 right-0 z-50 bg-white  ">
         <div>
           <img src={Logo} alt="DevStack" />
         </div>
